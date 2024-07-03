6d5c1576b55120f21c1e168ae2aa1b22923c6365
Oscar Acuña <oscarboris@outlook.cl>
2024-07-02 22:43:34 -0400

    carga inicial

---
+++ src/ConsultaPelicula.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import com.google.gson.Gson;
+public class ConsultaPelicula {
+
+    public Pelicula buscaPelicula(int numeroDePelicula){
+
+        URI direccion = URI.create("https://swapi.dev/api/films/"+numeroDePelicula);
+
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return new Gson().fromJson(response.body(), Pelicula.class);
+        } catch (Exception e) {
+            throw new RuntimeException("no encontré esa pelicula");
+        }
+
+
+    }
+}
